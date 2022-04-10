295a732c280c40d22fb0856b1b1dcbd67a85739d
Branden Palmer <brandenpalmer08@gmail.com>
2022-04-10 17:26:59 +0700

    implement capitalize module

--- modules/capitalize.js
+++ modules/capitalize.js
@@ -1,3 +1,14 @@
 export const capitalize = function (string) {
-  return "A";
+  const isSentence = string.includes(" ");
+  return isSentence ? capitalizedEveryWordOfSentence() : capitalizeWord(string);
+
+  function capitalizedEveryWordOfSentence() {
+    return string.split(" ").map(capitalizeWord).join(" ");
+  }
+
+  function capitalizeWord(word) {
+    const firstLetter = word[0];
+    const restOfWord = word.slice(1);
+    return firstLetter.toUpperCase() + restOfWord.toLowerCase();
+  }
 };

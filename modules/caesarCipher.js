94d1295e011f56a1b42b19a1d3ee22dc7f65ade3
Branden Palmer <brandenpalmer08@gmail.com>
2022-04-11 07:19:42 +0700

    implement caesarCipher

--- modules/caesarCipher.js
+++ modules/caesarCipher.js
@@ -1,3 +1,30 @@
 export function caesarCipher(string) {
-    return string;
+  return string.split("").map(shiftLetter).join("");
+
+  function shiftLetter(letter) {
+    if (letter === "z") return "a";
+    if (letter === "Z") return "A";
+    if (isLetter()) return shiftLetterToNextLetter(letter);
+    return letter;
+
+    function isLetter() {
+      const charCode = letter.charCodeAt();
+      const CHAR_CODE_OF_UPPERCASE_A = 64;
+      const CHAR_CODE_OF_UPPERCASE_Z = 90;
+      const CHAR_CODE_OF_LOWERCASE_A = 97;
+      const CHAR_CODE_OF_LOWERCASE_Z = 122;
+      const isUpperCaseLetter =
+        charCode >= CHAR_CODE_OF_UPPERCASE_A &&
+        charCode <= CHAR_CODE_OF_UPPERCASE_Z;
+      const isLowerCaseLetter =
+        charCode >= CHAR_CODE_OF_LOWERCASE_A &&
+        charCode <= CHAR_CODE_OF_LOWERCASE_Z;
+
+      return isUpperCaseLetter || isLowerCaseLetter;
+    }
+  }
+
+  function shiftLetterToNextLetter(letter) {
+    return String.fromCharCode(letter.charCodeAt() + 1);
+  }
 }
